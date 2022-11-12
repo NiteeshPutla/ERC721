a16eead46ddd0670aa1a784492ca09258f89caf0
nitesh <niteeshjoyal@gmail.com>
2022-11-12 14:55:45 +0530

    adding approval function

--- contracts/ERC721.sol
+++ contracts/ERC721.sol
@@ -8,14 +8,17 @@ contract NFTCollection {
     mapping (uint256=>address) internal _owners;
     mapping (address=>mapping(address=>bool)) private _operatorApprovals;
 
+    mapping(uint256=>address)private _tokenApprovals;
+
     event ApprovalForAll(address indexed _owner, address indexed operator,bool _approved);
-    function balanceOf(address _owner)external view returns(uint256){
+    event Approval(address indexed _owner,address indexed _approved,uint256 _tokenId);
+    function balanceOf(address _owner)public view returns(uint256){
         require(_owner!=address(0), "address is invalid");
         return _balances[_owner];
 
 
     }
-    function ownerOf(uint256 _tokenId)external view returns(address){
+    function ownerOf(uint256 _tokenId)public view returns(address){
         address owner=_owners[_tokenId];
         require(owner!=address(0), "Token id is not valid");
         return owner;
@@ -26,8 +29,16 @@ contract NFTCollection {
         emit ApprovalForAll(msg.sender,_operator,_approved);
     }
 
-    function isApprovalForAll(address _owner,address _operator)external view returns(bool){
+    function isApprovedForAll(address _owner,address _operator)public view returns(bool){
         return _operatorApprovals[_owner][_operator];
+
+    }
+
+    function approve(address _approved, uint256 _tokenId)external payable{
+        address owner = ownerOf(_tokenId);
+        require(msg.sender==owner || isApprovedForAll(owner,msg.sender),"Msg.sender is not the owner or operator");
+        _tokenApprovals[_tokenId]=_approved;
+        emit Approval(owner,_approved,_tokenId);
 
     }
 }
