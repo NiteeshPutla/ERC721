613fbf51c5010b700bd6ce13efa5a10d4226ec49
nitesh <niteeshjoyal@gmail.com>
2022-11-12 14:37:59 +0530

    writing approval functions

--- contracts/ERC721.sol
+++ contracts/ERC721.sol
@@ -6,6 +6,9 @@ contract NFTCollection {
 
     mapping (address=>uint256) internal _balances; 
     mapping (uint256=>address) internal _owners;
+    mapping (address=>mapping(address=>bool)) private _operatorApprovals;
+
+    event ApprovalForAll(address indexed _owner, address indexed operator,bool _approved);
     function balanceOf(address _owner)external view returns(uint256){
         require(_owner!=address(0), "address is invalid");
         return _balances[_owner];
@@ -17,4 +20,15 @@ contract NFTCollection {
         require(owner!=address(0), "Token id is not valid");
         return owner;
     }
+
+    function setApprovalForAll(address _operator,bool _approved)external{
+        _operatorApprovals[msg.sender][_operator] = _approved;
+        emit ApprovalForAll(msg.sender,_operator,_approved);
+    }
+
+    function isApprovalForAll(address _owner,address _operator)external view returns(bool){
+        return _operatorApprovals[_owner][_operator];
+
+    }
 }
+
