f9c18fac959653def006e34363500d66f0bc37bf
nitesh <niteeshjoyal@gmail.com>
2022-11-12 13:50:46 +0530

    Adding functions

--- contracts/ERC721.sol
+++ contracts/ERC721.sol
@@ -2,6 +2,19 @@
 pragma solidity^0.8.0;
 
 
-contract ERC721 {
-    
+contract NFTCollection {
+
+    mapping (address=>uint256) internal _balances; 
+    mapping (uint256=>address) internal _owners;
+    function balanceOf(address _owner)external view returns(uint256){
+        require(_owner!=address(0), "address is invalid");
+        return _balances[_owner];
+
+
+    }
+    function ownerOf(uint256 _tokenId)external view returns(address){
+        address owner=_owners[_tokenId];
+        require(owner!=address(0), "Token id is not valid");
+        return owner;
+    }
 }
