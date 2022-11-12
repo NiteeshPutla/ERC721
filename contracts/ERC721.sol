188ee53c15f9da3edbd71b8ccd35c2be2e9304ac
nitesh <niteeshjoyal@gmail.com>
2022-11-12 15:37:17 +0530

    Transfer function

--- contracts/ERC721.sol
+++ contracts/ERC721.sol
@@ -7,11 +7,13 @@ contract NFTCollection {
     mapping (address=>uint256) internal _balances; 
     mapping (uint256=>address) internal _owners;
     mapping (address=>mapping(address=>bool)) private _operatorApprovals;
-
     mapping(uint256=>address)private _tokenApprovals;
 
     event ApprovalForAll(address indexed _owner, address indexed operator,bool _approved);
     event Approval(address indexed _owner,address indexed _approved,uint256 _tokenId);
+    event Transfer(address indexed _from,address indexed _to,uint256 _tokenId);
+
+
     function balanceOf(address _owner)public view returns(uint256){
         require(_owner!=address(0), "address is invalid");
         return _balances[_owner];
@@ -34,12 +36,33 @@ contract NFTCollection {
 
     }
 
-    function approve(address _approved, uint256 _tokenId)external payable{
+    function approve(address _approved, uint256 _tokenId)public payable{
         address owner = ownerOf(_tokenId);
         require(msg.sender==owner || isApprovedForAll(owner,msg.sender),"Msg.sender is not the owner or operator");
         _tokenApprovals[_tokenId]=_approved;
         emit Approval(owner,_approved,_tokenId);
-
     }
+    function getApproved(uint256 _tokenId) public view returns(address){
+        require(_owners[_tokenId]!=address(0),"Token id is invalid");
+        return _tokenApprovals[_tokenId];
+    }
+    function transferFrom(address _from,address _to,uint256 _tokenId)external payable{
+        address owner=ownerOf(_tokenId);
+        require(msg.sender==owner || 
+        getApproved(_tokenId)==msg.sender ||isApprovedForAll(owner,msg.sender),
+        "Msg.sender is not the owner or approved address for transfer");
+
+        require(owner==_from,"From address is not the owner");
+        require(_to!=address(0),"Address is zero adress");
+
+        approve(address(0),_tokenId);
+
+        _balances[_from]-=1;
+        _balances[_to]+=1;
+        _owners[_tokenId]=_to;
+
+        emit Transfer(_from , _to, _tokenId);
+
+    }   
 }
 
